a68f04a9831bf74132cb9ac0366b60167c158743
dowrave <dowrave@gmail.com>
2024-08-07 19:44:16 +0900

    240807

--- Assets/Scripts/BottomPanelOperatorBox.cs
+++ Assets/Scripts/BottomPanelOperatorBox.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
-public class BottomPanelOperatorBox : MonoBehaviour
+public class BottomPanelOperatorBox : MonoBehaviour, IPointerClickHandler
 {
-    private Image operatorIcon;
-    private Text costText;
+    private Transform operatorIcon; // 오브젝트
+    private Image operatorIconImage; // Image 컴포넌트
+    private TextMeshProUGUI costText;
     private OperatorData operatorData; 
 
     public void Initialize(OperatorData data)
     {
         operatorData = data;
+        operatorIcon = transform.Find("OperatorIcon");
+
+        if (operatorIcon != null)
+        {
+            operatorIconImage = operatorIcon.GetComponentInChildren<Image>(); // OperatorIcon의 Image 컴포넌트에 접근
+            costText = GetComponentInChildren<TextMeshProUGUI>(); // CostText의 TextMeshPro에 접근
+        }
         UpdateVisuals();
     }
 
@@ -19,7 +28,8 @@ public class BottomPanelOperatorBox : MonoBehaviour
         // operatorData에 해당하는 아이콘이 있다면 그걸 넣고 
         if (operatorData.icon != null)
         {
-            operatorIcon.sprite = operatorData.icon;
+            operatorIconImage.sprite = operatorData.icon;
+            operatorIconImage.color = new Color(1f, 1f, 1f, 1f); // 일러스트에 변화를 주지 않고 그대로 넣음
         }
 
         // 없다면 operatorData에 해당하는 색깔만 가져와서 할당한다.
@@ -27,10 +37,8 @@ public class BottomPanelOperatorBox : MonoBehaviour
         {
             Renderer modelRenderer = operatorData.prefab.GetComponentInChildren<Renderer>();
             if (modelRenderer != null && modelRenderer.sharedMaterial != null)
-            {
-                Debug.Log($"{operatorIcon}");
-                
-                operatorIcon.color = modelRenderer.sharedMaterial.color;
+            {                
+                operatorIconImage.color = modelRenderer.sharedMaterial.color;
             }
         }
 
