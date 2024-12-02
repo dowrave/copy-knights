37f993862a319fbf317c94d1d5ea7ee1c50707f4
dowrave <dowrave@gmail.com>
2024-12-02 23:46:43 +0900

    241202

--- Assets/Scripts/Core/MainMenu/OperatorPromotionPanel.cs
+++ Assets/Scripts/Core/MainMenu/OperatorPromotionPanel.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OperatorPromotionPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Controls")]
+    [SerializeField] private Button confirmButton; 
+
+    private OwnedOperator op;
+
+    private void Awake()
     {
-        
+        confirmButton.onClick.AddListener(OnConfirmButtonClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(OwnedOperator op)
     {
-        
+        this.op = op;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        bool canPromote = op.CanPromote;
+        confirmButton.interactable = canPromote;
+    }
+
+    private void OnConfirmButtonClicked()
+    {
+        if (op.CanPromote)
+        {
+            OperatorGrowthManager.Instance.TryPromoteOperator(op);
+        }
     }
 }
